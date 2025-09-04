8d3be62eefaf46b448cd1074092c2951e8dd04b4
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-04 23:30:33 +0530

    just trying out this module

--- reminder.py
+++ reminder.py
@@ -1,1 +1,11 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
 
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("PyQt6 Test Window")
+window.resize(400, 300)
+window.show()
+
+app.exec()
